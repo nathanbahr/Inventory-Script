790fc5dd41f31221e2dd602455b7e5df9f575865
Nathan Bahr <nbgameboy@gmail.com>
2016-02-07 23:55:28 -0800

    Java and flash versions
    Started changing how version information is pulled for Java and Flash as
    not every system worked before. Added the ability to output NULL if
    Flash is not installed. Will continue to improve this and clean up the
    messy code around the if statements.

--- Inventory.ps1
+++ Inventory.ps1
@@ -19,7 +19,7 @@ $bios = Get-WmiObject Win32_bios
 $user = $env:username
 $date =  Get-Date -format s
 $os = Get-WmiObject Win32_operatingSystem
-$java = gp  'HKLM:\SOFTWARE\JavaSoft\Java Runtime Environment\'
+$java = Get-WmiObject -Class Win32_Product -Filter "Vendor like 'Oracle Corporation'" | Select -Expand Version -First 1 <#= gp  'HKLM:\SOFTWARE\JavaSoft\Java Runtime Environment\'#>
 $flash =  gp  'HKLM:\SOFTWARE\Macromedia\FlashPlayer\'
 $IE = gp 'HKLM:\SOFTWARE\Microsoft\Internet Explorer'
 
@@ -43,9 +43,14 @@ Inventory.csv: has most system and network information.
 Version.csv: has aplication and OS versions such as Firefox or Windows.
 '': represent a blank space to be manual filled in later#>
 Write-Output "$($id);$($hn);$($network.DHCPEnabled);$($network.IPAddress<#$ipv4add#>);$($network.IPSubnet);$($network.DefaultIPGateway);$($network.DNSServerSearchOrder);'';$($network.WINSPrimaryServer);$($network.WINSSecondaryServer);$($system.Domain);$($network.MACAddress);$($network.Description);$($netAdapter.AdapterType);'';'';'';'';'';$($user);'';$($system.Manufacturer);$($system.Model);'';'';$($bios.SerialNumber);'';$($memory)GB;$($system.SystemType);$($date)" >> Inventory\Inventory.txt
-Write-Output "$($id);$($hn);$($os.Version);$($bios.SMBIOSBIOSVersion);$($bios.Version);$($IE.Version);$(if ($firefox.publisher -eq "Mozilla") {Write-Output $firefox.DisplayVersion} else {Write-Output 'NULL'});$(if ($chrome.displayname -eq "Google Chrome") {Write-Output $chrome.Version} else {Write-Output 'NULL'});$($flash.CurrentVersion);$($java.CurrentVersion);$($java.BrowserJavaVersion);$($date);$($PSVersionTable.PSVersion);'';" >> Inventory\Version.txt
+Write-Output "$($id);$($hn);$($os.Version);$($bios.SMBIOSBIOSVersion);$($bios.Version);$($IE.Version);$(if ($firefox.publisher -eq "Mozilla") {Write-Output $firefox.DisplayVersion} else {Write-Output 'NULL'});$(if ($chrome.displayname -eq "Google Chrome") {Write-Output $chrome.Version} else {Write-Output 'NULL'});$(if ($flash.PSChildName -eq "FlashPlayer") {Write-Output $flash.CurrentVersion} else {Write-Output 'NULL'});$($java);$($java.BrowserJavaVersion);$($date);$($PSVersionTable.PSVersion);'';" >> Inventory\Version.txt
 
 #Makes three text files with detailed information about computer.
 Write-Output $ipconfig $netAdapter $route $date " " >> Inventory\details\$hn\detailedNetwork.txt
 Write-Output $hn $user $system $bios $date " " >> Inventory\details\$hn\detailedSystem.txt
 Write-Output $hn $os $bios $IE $firefox $chrome $flash $java $PSVersionTable $date " " >> Inventory\details\$hn\detailedVersion.txt
+
+
+
+
+#if ($java = "*") {Write-Output 'False'}
